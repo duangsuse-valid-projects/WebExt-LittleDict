91a66dd6bed1b57d6fa60b7393dc43d4e222606c
duangsuse <fedora-opensuse@outlook.com>
2020-11-08 05:15:07 +0800

    Refine project struct

--- gen_manifest.js
+++ gen_manifest.js
@@ -5,14 +5,19 @@ const gitUrl = name => RegExp(`\\[remote "origin"\\]
 \\s*url = (\\S*)
 `, "gm").exec(readFileSync(".git/config").toString())[1];
 function iconNames(xs/*size,N*/, name="ic") {
+  const getStdout = cmd => require("child_process").execSync(cmd).toString();
   let obj = {};
-  for (let x of xs) obj[x] = `icons/${name}_${x}.png`;
+  for (let x of xs) {
+    obj[x] = `icons/${name}_${x}.png`;
+    getStdout(`convert resources/icon.png -resize ${x}x${x} build/${obj[x]}`);
+  }
   return obj;
 }
+function capitalize(s) { return (s!="")? s[0].toUpperCase()+s.substr(1) : ""; }
 
 const manifest = k_ => { return {
   manifest_version: 2,
-  name: k_("name"),
+  name: k_("name").split(/[-_]/).map(capitalize).join(""),
   version: k_("version"),
   description: k_("description"),
   homepage_url: gitUrl("origin").replace(".git", ""),
